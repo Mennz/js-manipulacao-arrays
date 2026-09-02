ab5a7f0b7de5644651e2d437a9b7700e9003806b
Mennz <davi.mendoza.rezende.c@gmail.com>
2026-09-02 11:01:52 -0300

    exercicios com map e filter

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const { alunos, produtos } = require("./dados");
 
-console.log(alunos);
-console.log(produtos);
+const nomesAlunos = alunos.map((a) => a.nome);
+console.log("nomes:", nomesAlunos);
+
+// map cria array novo, o original nao muda
+const precosComDesconto = produtos.map((p) => ({
+  ...p,
+  preco: p.preco * 0.9,
+}));
+console.log("com desconto:", precosComDesconto);
+
+const aprovados = alunos.filter((a) => a.nota >= 6);
+console.log("aprovados:", aprovados);
+
+const estoqueBaixo = produtos.filter((p) => p.estoque < 5);
+console.log("estoque baixo:", estoqueBaixo);
